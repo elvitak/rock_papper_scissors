b444f877d818ef78a376ccdb6a421b97cbfa510c
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-03 17:39:30 +0100

    create specs for all scenarios win, lose and draw

---
+++ cypress/integration/userCanPlayAgainstComputer.feature.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+describe("User", () => {
+  before(() => {
+    cy.visit("http://localhost:3000", {
+      onBeforeLoad(window) {
+        cy.stub(window.Math, "floor").returns(0);
+      },
+    });
+  });
+
+  it("is expected to win when chooses paper and computer chooses rock", () => {
+    cy.get("[data-cy=paper_btn]").click();
+    cy.get("[data-cy=user_choice]").should("contain", "Your choice is: Paper");
+    cy.get("[data-cy=computerChoice]").should(
+      "contain",
+      "Your Imaginary Friend chose: Rock"
+    );
+    cy.get("[data-cy=results]").should("contain", "Congratulations! You won!");
+  });
+
+  it("is expected to lose when chooses scissors and computer chooses rock", () => {
+    cy.get("[data-cy=scissors_btn]").click();
+    cy.get("[data-cy=user_choice]").should(
+      "contain",
+      "Your choice is: Scissors"
+    );
+    cy.get("[data-cy=computerChoice]").should(
+      "contain",
+      "Your Imaginary Friend chose: Rock"
+    );
+    cy.get("[data-cy=results]").should("contain", "You lost");
+  });
+
+  it("is expected to see result as a draw when chooses rock and computer chooses rock", () => {
+    cy.get("[data-cy=rock_btn]").click();
+    cy.get("[data-cy=user_choice]").should("contain", "Your choice is: Rock");
+    cy.get("[data-cy=computerChoice]").should(
+      "contain",
+      "Your Imaginary Friend chose: Rock"
+    );
+    cy.get("[data-cy=results]").should("contain", "It's a draw");
+  });
+});
