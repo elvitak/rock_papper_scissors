d8318ff199b2e52cd0407b95e096331a430186ce
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-03 17:26:06 +0100

    add functionality behind computers choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+/* eslint-disable no-undef */
 import React, { useState } from "react";
 
 const App = () => {
   const [userChoice, setUserChoice] = useState();
+  const [computerChoice, setComputerChoice] = useState();
 
   const handleClick = (value) => {
     setUserChoice(value);
+    generateComputersChoice();
+  };
+
+  const generateComputersChoice = () => {
+    const options = ["Rock", "Paper", "Paper"];
+    setComputerChoice(options[Math.floor(Math.random * 3)]);
   };
 
   return (
     <>
       <h1 data-cy="user_choice">Your choice is: {userChoice}</h1>
+      <h1 data-cy="computerChoice">
+        Your Imaginary Friend chose: {computerChoice}
+      </h1>
       <button data-cy="rock_btn" onClick={() => handleClick("Rock")}>
         Rock
       </button>
